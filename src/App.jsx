ded13872ecb1b5855ade3b65d9ee4ef09255a3d4
Luca Hazuca <lhazuca.contractor@lattice.com>
2024-08-30 16:05:57 -0300

    setup fs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {isInitialized} from "@fullstory/browser";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,13 +11,13 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React {isInitialized() && '+ FullStory'}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
